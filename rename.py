aebacc3c2ebe6391f589e87177e02586f615e220
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-02-21 10:48:39 -0300

    Rename folders and skip unwanted files and folders

--- rename.py
+++ rename.py
@@ -11,8 +11,8 @@ with open('games_list.txt') as f:
   games_list = f.readlines()
 
 for game in games_list:
- game_clean = re.sub('\n', '', game)
- games.append(game_clean)
+  game_clean = re.sub('\n', '', game)
+  games.append(game_clean)
 
 directory = os.path.dirname(os.path.realpath(sys.argv[0]))
 
@@ -21,18 +21,15 @@ array_to_rename = []
 
 count = 0
 
-for subdir, dirs, files in os.walk(directory):
- for full_filename in files:
-  if full_filename == 'games_list.txt' or full_filename == 'main.py' or full_filename =='spyder.cpython-39.pyc':
-      continue
+def rename(full_filename, current_directory, count):
   filename, file_extension = os.path.splitext(full_filename)
 
   highest = process.extractOne(filename, games)
 
   if highest[1] == 100:
-      continue
+    return
   if highest[1] >= 90:
-    subdirectory_path = os.path.relpath(subdir, directory)
+    subdirectory_path = os.path.relpath(subdir, current_directory)
 
     file_path_old = os.path.join(subdirectory_path, full_filename)
 
@@ -43,6 +40,25 @@ for subdir, dirs, files in os.walk(directory):
     array_to_rename.append(file_path_old)
     print(count, ") ", file_path_old , "------>", map_to_rename[file_path_old], "score:", highest[1])
     count = count + 1
+
+blacklist_folders = ['.mypy_cache', '.git']
+blacklist_files = ['rename.py', 'spyder.py', 'games_list.txt', 'requirements.txt', '.gitignore']
+
+for subdir, dirs, files in os.walk(directory):
+  contains = False
+  for blacklist_folder in blacklist_folders:
+    if blacklist_folder in subdir:
+      contains = True
+  if contains:
+    continue
+  for directory in dirs:
+    if directory in blacklist_folders:
+      continue
+    rename(directory, subdir, count)
+  for full_filename in files:
+    if full_filename in blacklist_files:
+      continue
+    rename(full_filename, subdir, count)
 
 if array_to_rename:
   print("Please enter the numbers separeted by spaces of the renames that you wish to skip! Or enter -1 to rename nothing. Or just hit enter to apply all renames.")
