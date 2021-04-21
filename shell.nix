20d00de8f5f91783dd56552986e5b975463d6ef0
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-04-21 13:02:18 -0300

    Fix commit for pkgs

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
   unstable = import
-    (builtins.fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) { };
+    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/93184413f2180ce79d53df91df4d43c2e8f931aa.tar.gz) { };
 in
 unstable.mkShell {
   buildInputs = [
